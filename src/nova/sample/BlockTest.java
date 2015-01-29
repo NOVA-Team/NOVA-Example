6bbcdc3ad7041dd0833dc33445fee3c2de038f9c
Calclavia <admin@calclavia.com>
2015-01-29 20:34:30 +0800

    Added onRightClick test

--- src/nova/sample/BlockTest.java
+++ src/nova/sample/BlockTest.java
@@ -1,13 +1,21 @@
 package nova.sample;
 
 import nova.core.block.Block;
-import nova.core.block.BlockManager;
+import nova.core.entity.Entity;
+import nova.core.util.transform.Vector3d;
 
 /**
+ * Literarlly, this is a test block.
  * @author Calclavia
  */
 public class BlockTest extends Block {
-	
+
+	@Override
+	public boolean onRightClick(Entity entity, int side, Vector3d hit) {
+		System.out.println("I'm being right clicked!");
+		return true;
+	}
+
 	@Override
 	public String getID() {
 		return "test";
