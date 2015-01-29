dbfb8375ee8111be40548303758503b773233c80
Calclavia <admin@calclavia.com>
2015-01-29 20:22:11 +0800

    Initial work on NovaLauncher and successfully launched Minecraft

--- src/nova/sample/NovaTest.java
+++ src/nova/sample/NovaTest.java
@@ -2,6 +2,7 @@ package nova.sample;
 
 import nova.core.block.Block;
 import nova.core.game.Game;
+import nova.core.loader.Loadable;
 import nova.core.loader.NovaMod;
 
 /**
@@ -9,12 +10,12 @@ import nova.core.loader.NovaMod;
  * @author Calclavia
  */
 @NovaMod(id = "Nova Test", name = "Nova Test", version = "0.0.1", novaVersion = "0.0.1")
-public class NovaTest {
+public class NovaTest implements Loadable {
 
-	public final NovaTest instance = new NovaTest();
 	public final Block blockTest = new BlockTest();
 
-	public void init() {
+	@Override
+	public void preInit() {
 		Game.instance.get().blockManager.registry.register(blockTest);
 	}
 }
