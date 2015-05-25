13120539408cae3f9c6ffe8d5b1d4f6acd841240
Calclavia <admin@calclavia.com>
2015-05-25 21:56:41 +0800

    EntityCollider and Damageable are now components

--- src/main/java/nova/sample/entity/EntityMovableSimpleTest.java
+++ src/main/java/nova/sample/entity/EntityMovableSimpleTest.java
@@ -1,24 +1,25 @@
 package nova.sample.entity;
 
 import nova.core.component.Updater;
+import nova.core.component.misc.Collider;
 import nova.core.entity.Entity;
-import nova.core.entity.component.Collidable;
 import nova.core.network.Sync;
 import nova.core.render.model.Model;
 import nova.core.retention.Storable;
 import nova.core.retention.Stored;
 import nova.core.util.transform.matrix.Quaternion;
-import nova.core.util.transform.shape.Cuboid;
 import nova.sample.NovaTest;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
-public class EntityMovableSimpleTest extends Entity implements Collidable, Storable, Updater {
+public class EntityMovableSimpleTest extends Entity implements Storable, Updater {
 
 	@Stored
 	@Sync
-	private double excited,timer;
+	private double excited, timer;
+
+	public EntityMovableSimpleTest() {
+		add(new Collider())
+			.setEntityCollide(other -> excited = 1);
+	}
 
 	public void render(Model model) {
 		Model grinderModel = NovaTest.grinderModel.getModel();
@@ -26,9 +27,9 @@ public class EntityMovableSimpleTest extends Entity implements Collidable, Stora
 		grinderModel
 			.combineChildren("crank", "crank1", "crank2", "crank3")
 			.rotate(Quaternion.fromEuler(0, -timer, 0));
-		
+
 		grinderModel.translate(0, 0.5, 0);
-		
+
 		model.children.add(grinderModel);
 		model.bindAll(NovaTest.grinderEntityTexture);
 	}
@@ -39,24 +40,13 @@ public class EntityMovableSimpleTest extends Entity implements Collidable, Stora
 	}
 
 	@Override
-	public Collection<Cuboid> getCollisionBoxes() {
-		ArrayList<Cuboid> cuboids = new ArrayList<Cuboid>(1);
-		cuboids.add(new Cuboid(-0.5, 0, -0.5, 0.5, 1, 0.5));
-		return cuboids;
-	}
-
-	@Override
-	public void onCollide(Entity collidedWith) {
-		excited = 1;
-	}
-
-	@Override
 	public void update(double deltaTime) {
 		excited -= deltaTime / 4.0d;
-		if (excited < 0.1)
+		if (excited < 0.1) {
 			excited = 0.1;
+		}
 		timer += deltaTime * excited;
-		NovaTest.checkConversion(this,"EntityMovableSimpleTest");
+		NovaTest.checkConversion(this, "EntityMovableSimpleTest");
 	}
 
 }
