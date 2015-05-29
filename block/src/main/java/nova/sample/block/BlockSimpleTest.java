f6feb0e00e5d06b0224ce6930c33138482759424
Łukasz Magiera <magik6k@gmail.com>
2015-05-29 21:03:55 +0200

    Added block example
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

---
+++ block/src/main/java/nova/sample/block/BlockSimpleTest.java
@@ -0,0 +1,50 @@
+package nova.sample.block;
+
+import nova.core.block.Block;
+import nova.core.block.component.StaticBlockRenderer;
+import nova.core.component.Category;
+import nova.core.component.misc.Collider;
+import nova.core.component.renderer.ItemRenderer;
+import nova.core.game.Game;
+import nova.core.network.Packet;
+import nova.core.network.PacketHandler;
+
+import java.util.Optional;
+
+/**
+ * Literally, this is a test block.
+ * @author Calclavia
+ */
+public class BlockSimpleTest extends Block implements PacketHandler {
+
+	public BlockSimpleTest() {
+		add(new StaticBlockRenderer(this)).setTexture((dir) -> Optional.of(NovaBlock.steelTexture));
+
+		add(new Collider());
+
+		add(new ItemRenderer(this));
+
+		add(new Category("buildingBlocks"));
+		rightClickEvent.add(this::onRightClick);
+	}
+
+	public void onRightClick(RightClickEvent evt) {
+		System.out.println("Sending Packet: 1234");
+		Game.instance.networkManager.sync(this);
+	}
+
+	@Override
+	public void read(Packet packet) {
+		System.out.println("Received packet: " + packet.readInt());
+	}
+
+	@Override
+	public void write(Packet packet) {
+		packet.writeInt(1234);
+	}
+
+	@Override
+	public String getID() {
+		return "simple";
+	}
+}
---
+++ block/src/main/java/nova/sample/block/NovaBlock.java
@@ -0,0 +1,57 @@
+package nova.sample.block;
+
+import nova.core.block.BlockFactory;
+import nova.core.block.BlockManager;
+import nova.core.game.Game;
+import nova.core.item.ItemFactory;
+import nova.core.item.ItemManager;
+import nova.core.loader.Loadable;
+import nova.core.loader.NovaMod;
+import nova.core.nativewrapper.NativeManager;
+import nova.core.recipes.crafting.ItemIngredient;
+import nova.core.recipes.crafting.ShapedCraftingRecipe;
+import nova.core.render.RenderManager;
+import nova.core.render.texture.BlockTexture;
+
+/**
+ * A test Nova Mod
+ * 
+ * @author Calclavia
+ */
+@NovaMod(id = NovaBlock.id, name = "Nova Example Block", version = "0.0.1", novaVersion = "0.0.1")
+public class NovaBlock implements Loadable {
+
+	public static final String id = "novablockexample";
+
+	public static BlockFactory blockTest;
+	public static ItemFactory itemBlockTest;
+
+	public static BlockTexture steelTexture;
+
+	public final BlockManager blockManager;
+	public final ItemManager itemManager;
+	public final RenderManager renderManager;
+	public final NativeManager nativeManager;
+
+	public NovaBlock(BlockManager blockManager, ItemManager itemManager, RenderManager renderManager, NativeManager nativeManager) {
+		this.blockManager = blockManager;
+		this.itemManager = itemManager;
+		this.renderManager = renderManager;
+		this.nativeManager = nativeManager;
+	}
+
+	@Override
+	public void preInit() {
+		blockTest = blockManager.register(BlockSimpleTest.class);
+
+		itemBlockTest = itemManager.getItemFromBlock(blockTest);
+
+		steelTexture = renderManager.registerTexture(new BlockTexture(id, "blockSteel"));
+
+		// try to add a recipe
+		ItemIngredient stickIngredient = ItemIngredient.forItem("minecraft:stick"); //TODO: This should be obtained from some dictonary too
+		ItemIngredient ingotIngredient = ItemIngredient.forDictionary("ingotIron");
+
+		Game.instance.recipeManager.addRecipe(new ShapedCraftingRecipe(itemBlockTest.makeItem(), "AAA-ABA-AAA", ingotIngredient, stickIngredient));
+	}
+}
