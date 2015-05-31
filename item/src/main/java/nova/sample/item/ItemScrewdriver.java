00edc121e9bf3c4f87847b6339d428329d0f770c
Łukasz Magiera <magik6k@gmail.com>
2015-05-31 12:08:25 +0200

    Fixed item example
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

--- item/src/main/java/nova/sample/item/ItemScrewdriver.java
+++ item/src/main/java/nova/sample/item/ItemScrewdriver.java
@@ -1,14 +1,8 @@
 package nova.sample.item;
 
 import nova.core.component.Category;
-import nova.core.entity.Entity;
 import nova.core.item.Item;
 import nova.core.render.texture.ItemTexture;
-import nova.core.util.Direction;
-import nova.core.util.transform.vector.Vector3d;
-import nova.core.util.transform.vector.Vector3i;
-import nova.core.world.World;
-
 import java.util.Optional;
 
 /**
@@ -18,13 +12,9 @@ public class ItemScrewdriver extends Item {
 
 	public ItemScrewdriver() {
 		add(new Category("tools"));
+		useEvent.add(event -> event.action = true);
 	}
-
-	@Override
-	public boolean onUse(Entity entity, World world, Vector3i position, Direction side, Vector3d hit) {
-		return true;
-	}
-
+	
 	@Override
 	public Optional<ItemTexture> getTexture() {
 		return Optional.of(NovaItem.screwTexture);
