d55325281971e5c38398ecc6cfaf275af1f12233
Calclavia <admin@calclavia.com>
2015-07-27 23:57:31 -0400

    Migrate NOVA-GUI into a separate plugin

--- gui/src/main/java/nova/sample/gui/NovaGui.java
+++ gui/src/main/java/nova/sample/gui/NovaGui.java
@@ -18,7 +18,6 @@ import nova.core.item.ItemFactory;
 import nova.core.item.ItemManager;
 import nova.core.loader.Loadable;
 import nova.core.loader.NovaMod;
-import nova.core.nativewrapper.NativeManager;
 import nova.core.network.NetworkManager;
 import nova.core.network.NetworkTarget.Side;
 import nova.core.recipes.RecipeManager;
@@ -52,11 +51,11 @@ public class NovaGui implements Loadable {
 	public final RecipeManager recipeManager;
 
 	public NovaGui(BlockManager blockManager,
-				   ItemManager itemManager,
-				   RenderManager renderManager,
-				   GuiManager guiFactory,
-				   RecipeManager recipeManager,
-				   NetworkManager networkManager) {
+		ItemManager itemManager,
+		RenderManager renderManager,
+		GuiManager guiFactory,
+		RecipeManager recipeManager,
+		NetworkManager networkManager) {
 		this.blockManager = blockManager;
 		this.itemManager = itemManager;
 		this.renderManager = renderManager;
@@ -69,36 +68,36 @@ public class NovaGui implements Loadable {
 
 	public static void initializeGUI() {
 		guiFactory.register(() -> new Gui("testgui")
-			.add(new Button("testbutton2", "I'm EAST")
-				.setMaximumSize(Integer.MAX_VALUE, 120)
+				.add(new Button("testbutton2", "I'm EAST")
+					.setMaximumSize(Integer.MAX_VALUE, 120)
 
-				.onEvent((event, component) -> {
-					System.out.println("Test button pressed! " + Side.get());
-				}, ActionEvent.class, Side.BOTH), Anchor.EAST)
+					.onEvent((event, component) -> {
+						System.out.println("Test button pressed! " + Side.get());
+					}, ActionEvent.class, Side.BOTH), Anchor.EAST)
 
-			.add(new Button("testbutton3", "I'm CENTER"))
-			.add(new Container("test").setLayout(new FlowLayout())
-				.add(new Slot("main", 0))
-				.add(new Slot("main", 0))
-				.add(new Slot("main", 0))
-				.add(new Slot("main", 0))
-			, Anchor.SOUTH)
+				.add(new Button("testbutton3", "I'm CENTER"))
+				.add(new Container("test").setLayout(new FlowLayout())
+					.add(new Slot("main", 0))
+					.add(new Slot("main", 0))
+					.add(new Slot("main", 0))
+					.add(new Slot("main", 0))
+					, Anchor.SOUTH)
 
-			.add(new Container("container").setLayout(new FlowLayout())
-				.add(new Button("testbutton5", "I'm the FIRST Button and need lots of space"))
-				.add(new Label("testlabel1", "I'm some label hanging around").setBackground(new Background(Color.white)))
-				.add(new Button("testbutton7", "I'm THIRD"))
-				.add(new Button("testbutton8", "I'm FOURTH"))
-			, Anchor.NORTH)
+				.add(new Container("container").setLayout(new FlowLayout())
+					.add(new Button("testbutton5", "I'm the FIRST Button and need lots of space"))
+					.add(new Label("testlabel1", "I'm some label hanging around").setBackground(new Background(Color.white)))
+					.add(new Button("testbutton7", "I'm THIRD"))
+					.add(new Button("testbutton8", "I'm FOURTH"))
+					, Anchor.NORTH)
 
-			.onGuiEvent((event) -> {
-				event.gui.addInventory("main", ((BlockSimpleTest)event.block.get()).inventory);
-				System.out.println("Test GUI initialized! " + event.player.getDisplayName() + " " + event.position);
-			}, BindEvent.class)
+				.onGuiEvent((event) -> {
+					event.gui.addInventory("main", ((BlockSimpleTest) event.block.get()).inventory);
+					System.out.println("Test GUI initialized! " + event.player.getDisplayName() + " " + event.position);
+				}, BindEvent.class)
 
-			.onGuiEvent((event) -> {
-				System.out.println("Test GUI closed!");
-			}, UnBindEvent.class)
+				.onGuiEvent((event) -> {
+					System.out.println("Test GUI closed!");
+				}, UnBindEvent.class)
 		);
 	}
 
